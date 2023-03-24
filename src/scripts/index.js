b8b594739cd6e655a0007bbc3e3af8a25dfe5ad6
GHOST-Aram <owinoojala@gmail.com>
2023-03-24 10:17:34 +0300

    feat: Ftech background image from giphy api

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,3 +1,20 @@
 import '../styles/styles.css'
 const title = document.querySelector('title')
 title.textContent = title.textContent.concat(`in ${'Nairobi'.toLocaleUpperCase()}`)
+
+//Fetc background Image
+async function fetchBackgroundImage (objectName) {
+    try {
+        const response = await  fetch(`https://api.giphy.com/v1/gifs/translate?api_key=982nFUcRZilPuQGU5jWLUZTeRq0yEdpT&s=${objectName}`,
+        {mode: 'cors'})
+        const data = await response.json()
+
+        const dataSet = await data
+        
+        document.body.style.backgroundImage = `url(${dataSet.data.images.original.url})`
+        
+    } catch (error) {
+        console.log(` Image not found ${error}`)
+    }
+}
+fetchBackgroundImage('windy')
